59421f7211da4805453071e634bde2c866929c4c
ksomville <kirgan.somville@hetic.net>
2015-04-15 16:49:13 +0200

    form

--- easy-travel/client/controller/add-travel.js
+++ easy-travel/client/controller/add-travel.js
@@ -1,4 +1,7 @@
 // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+
+AddTravel = new Mongo.Collection("add_travel");
+
 Template["add-travel"].events({
   "submit .add-travel": function (event) {
     // This function is called when the new task form is submitted
@@ -14,26 +17,19 @@ Template["add-travel"].events({
     var place = target.place.value;
     var car_model = target.car_model.value;
 
+    AddTravel.insert({
 
-    Meteor.users.update({ _id : Meteor.userId }, {
-
-      $set: {
-              proposed_travel: [{
-
-                _id: new Mongo.ObjectID(),
-                createdAt: new Date(), // current time
-                departure_city: departure_city,
-                departure_place: departure_place,
-                arrival_city: arrival_city,
-                arrival_place: arrival_place,
-                departure_date: departure_date,
-                departure_time: departure_time,
-                price: price,
-                place: place,
-                car_model: car_model
-
-              }]
-          }
+      createdBy : Meteor.userId(),
+      createdAt: new Date(), // current time
+      departure_city: departure_city,
+      departure_place: departure_place,
+      arrival_city: arrival_city,
+      arrival_place: arrival_place,
+      departure_date: departure_date,
+      departure_time: departure_time,
+      price: price,
+      place: place,
+      car_model: car_model
 
     });
 
