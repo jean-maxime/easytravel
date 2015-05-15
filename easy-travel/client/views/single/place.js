ac0c31f5e87f28479c442d4e8357d71cd7496006
Jean-Maxime <jean-max@wanadoo.fr>
2015-05-15 14:04:31 +0200

    liste de mes voyages

--- easy-travel/client/views/single/place.js
+++ easy-travel/client/views/single/place.js
@@ -1,35 +1,28 @@
-// Inside the if (Meteor.isClient) block, right after Template.body.helpers:
-
 Template.singleId.events({
   "submit form": function (event) {
 
-    // This function is called when the new task form is submitted
-    // console.log('tets');
-    // var target = document.getElementById( "add-travel" );
     var travel_id = event.target.travel_id.value;
     var places_taken = event.target.places_taken.value;
     var places_free = event.target.places_free.value;
     var places = places_free - places_taken;
     var user_id = event.target.user_id.value;
+    var departure_date = event.target.departure_date.value;
+    var departure_time = event.target.departure_time.value;
+    var departure_city = event.target.departure_city.value;
+    var departure_place = event.target.departure_place.value;
+    var arrival_city = event.target.arrival_city.value;
+    var arrival_place = event.target.arrival_place.value;
+    var price = event.target.price.value;
 
-    AddTravel.update({_id: travel_id}, {$set: {place: places}});
-    
-    // User.createIndex({id: 1}, { unique: true });
-    // If user exist -> update else -> create
-    // User.insert({id: user_id, travel: [{id: travel_id, places: places_taken}]});
-    // User.update({id: user_id}, upsert: true);
+    // Add travel in collection
+    Meteor.call('travelUpdate', travel_id, places);
 
-    // User.update(
-    //   { id: user_id },
-    //   {$push: {
-    //     travel: {id: travel_id, places: places_taken}
-    //   }}, {upsert: true}
-    // );
-
-    Meteor.call('userUpdate', user_id, travel_id,places_taken);
+    // Add travel to user collection
+    Meteor.call('userUpdate', user_id, travel_id, places_taken, departure_date, departure_time, departure_city, departure_place, arrival_city, arrival_place, price);
   
     // Prevent default form submit
     event.preventDefault();
+    Router.go('/');
     return false;
   }
 
@@ -41,12 +34,4 @@ Template.singleId.events({
   "click button":function(){
     $('#place').fadeIn();
   }
-
-
 });
-
-// Meteor.startup(function() {
-//   $('.book').click(function(){
-//     alert('test')
-//   })
-// });
